1c94df5a332eaac62c6874b88dccc6666cba97ff
enjie ji <enjie.ji@mywali.co>
2019-10-10 16:25:35 -0500

    template added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// new Promise((resolve,reject) => {
+//     return reject(new Error('No bears'));
+
+//     setTimeout(() => {
+//         resolve('Bears, Beets, Battlestar');
+//         reject('')
+//     },2000);
+// })
+// .then(quote => {
+//     console.log(quote);
+// })
+// .catch(error=>console.log('error',error));
